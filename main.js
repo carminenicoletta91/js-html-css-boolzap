31f9ce8baf27eb5c2d4062a046cf42457637f304
carminenicoletta91 <carminenicolettakr91@gmail.com>
2019-07-22 22:16:19 +0200

    --creata funzione per inserire messaggio nella chat-- --aggiunti elementi grafici--

---
+++ main.js
@@ -0,0 +1,23 @@
+// creare funzione per scrivere messaggio e mandarlo nella chat
+$( document ).ready(function() {
+
+  $(".send-message a i.send ").click(
+    // funzione per inserire il messaggio nella chat
+    function(){
+      var inputval = $("#msginput").val();
+      console.log(inputval);
+      // inserisco il mio testo nell'apposita sezione contenente classe utente-verde
+      $(".chat").append(
+        "<section class='utente-verde'>" + "<p>"+ inputval +"</p>" + "</section>"
+      )
+      // pulisco l'input quando invio
+      $("#msginput").val("");
+      //genero una risposta ad ogni invio
+      $(".chat").append(
+        "<section class='utente-bianco'>" + "<p>"+ "ok" +"</p>" + "</section>"
+      )
+
+
+})
+
+});
