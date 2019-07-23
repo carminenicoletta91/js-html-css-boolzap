7b0a7f74265a8bdf241361792ad969850c925874
carminenicoletta91 <carminenicolettakr91@gmail.com>
2019-07-23 22:10:23 +0200

    --creata funzione di ricerca nome nella chat quando inserisco dei valori--

--- main.js
+++ main.js
@@ -9,18 +9,57 @@ $( document ).ready(function() {
       // inserisco il mio testo nell'apposita sezione contenente classe utente-verde
       $(".chat").append(
         "<section class='utente-verde'>" + "<p>"+ inputval +"</p>" + "</section>"
-      )
+      );
       // pulisco l'input quando invio
       $("#msginput").val("");
       //genero una risposta ad ogni invio
       function testobianco(){
       $(".chat").append(
         "<section class='utente-bianco'>" + "<p>"+ "ok" +"</p>" + "</section>"
-      )
+      );
       console.log(testobianco);
     }
     setTimeout(testobianco,1000);
 
-})
+  });
+    // richiamo una funzione quando premo tasti sul mio input
+    $(".search .search-input input").keypress(
+
+    // funzione di ricerca
+    function search(){
+      // nascondo tutte le mie sezioni contenenti le chat
+      $(".chat-name section").hide();
+      // scorro nei nomi delle chat
+      $(".chat-name section .message-name h2" ).each(function( index) {
+        var trova;
+        // creo oggetto che contiene il valore che digito in input
+        var inputval = $(".search .search-input input").val();
+        console.log(inputval);
+        // creo oggetto che contiene il nome della chat
+        var nametemp = $(this).text();
+        console.log(nametemp);
+        // assegno l'indice dell'oggetto alla variabile trova
+        trova = index;
+        // se questo oggetto contiene i valori del mio input
+        if( nametemp.includes(inputval)){
+          console.log("sei qui:" + trova);
+          // scorro nelle varie sezioni
+          $(".chat-name section").each(function(index)
+
+          {
+            // se l'indice della mia sezione è uguale a quello del mio nome chat
+            if(index === trova){
+              console.log(this);
+              // mostro quella sezione
+              $(this).show();
+            }
+
+          });//chiusura each per le sezioni
+        }
+
+      });//chiusura each lettura nome chat
+
+});//chiusura funzione search al keypress
+
 
 });
